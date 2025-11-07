f7e15d093e642788339a8bf4ce2c0d5da2e35a01
immanuel <emmanuelmendoza448@gmail.com>
2025-11-07 15:04:30 -0600

    hash fijo

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php
+// includes/db.php
+// --- MODO A: HASH FIJO EN CÓDIGO ---
+// Genera tu propio hash con: php -r "echo password_hash('TU_CLAVE', PASSWORD_DEFAULT), PHP_EOL;"
+
+return [
+    'mode' => 'hash',
+    'password_hash' => '$2y$10$0V4d35yWfExampleExampleExamplefP8G2f7M1YwW4i1fmvJ5rC4y' // <-- REEMPLAZA POR TU HASH
+];
