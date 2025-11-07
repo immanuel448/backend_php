1abd3061476f6b953f98e65f1e7d5bd2d7dd1f6a
immanuel <emmanuelmendoza448@gmail.com>
2025-11-07 15:07:05 -0600

    se crea salir.php

---
+++ salir.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Elimina variables y destruye la sesión
+$_SESSION = [];
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_destroy();
+
+// Redirige al login
+header("Location: acceso.php");
+exit();
